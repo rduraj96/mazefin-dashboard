68e27967136b1acea0dfce8a79f38b7c2160a05d
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-03-28 21:07:09 -0400

    first commit

---
+++ client/src/state/types.ts
@@ -0,0 +1,58 @@
+export interface ExpensesByCatagory {
+    salaries: number;
+    supplies: number;
+    services: number;
+}
+
+export interface Month {
+    id: string;
+    month: string;
+    revenue: number;
+    expenses: number;
+    nonOperationalExpenses: number;
+    operationalExpenses: number;
+}
+
+export interface Day {
+    id: string;
+    date: string;
+    revenue: number;
+    expenses: number,
+}
+
+export interface GetKpisResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    totalProfit: number;
+    totalRevenue: number;
+    totalExpenses: number;
+    expensesByCategory: ExpensesByCatagory;
+    monthlyData: Array<Month>;
+    dailyData: Array<Day>
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface GetProductsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<String>;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface GetTransactionsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    buyer: string;
+    amount: number;
+    productIds: Array<String>;
+    createdAt: string;
+    updatedAt: string;
+}
+
